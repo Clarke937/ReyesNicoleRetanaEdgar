6bc0b1a7fa8360d44b722c2d9de5e7381ea4d000
Clarke937 <eretana60@gmail.com>
2022-03-26 15:49:08 -0600

    Sexto

--- database/seeders/InvestigationTagSeeder.php
+++ database/seeders/InvestigationTagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Investigation_Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,35 @@ class InvestigationTagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Investigation_Tag::insert([
+            [
+                'id' => 1,
+                'investigations_id' => 1,
+                'tag_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'id' => 2,
+                'investigations_id' => 2,
+                'tag_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'id' => 3,
+                'investigations_id' => 3,
+                'tag_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'id' => 4,
+                'investigations_id' => 4,
+                'tag_id' => 4,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
     }
 }
